ef80b57fd54a434f840334b2e7ef5012ee0c54ec
riteshdhodakiya-sketch <ritesh.dhodakiya@acldigital.com>
2025-09-10 17:49:36 +0530

    minor fix

--- SwiftUIDemo/SwiftUIDemoTests/SwiftUIDemoTests.swift
+++ SwiftUIDemo/SwiftUIDemoTests/SwiftUIDemoTests.swift
@@ -13,8 +13,6 @@ struct SwiftUIDemoTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-        let result = 2 + 2
-        XCTAssertEqual(result, 4, "Math is broken!")
     }
 
     func testExample() throws {
